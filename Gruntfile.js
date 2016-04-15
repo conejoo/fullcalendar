da3468bd40a97a9dc7ed3d8e737e2bbd152c0cee
conejoo <waldoc@gmail.com>
2016-04-15 15:45:06 -0300

    Changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,6 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-jscs-checker');
 	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-bump');
 	grunt.loadNpmTasks('lumbar');
 	
@@ -43,7 +42,6 @@ module.exports = function(grunt) {
 		'clean',
 		'modules',
 		'languages',
-		'karma:single',
 		'archiveDist'
 	]);
 
